c50c6bf90e06d34dcf7f4f3aafc76de9e47455a4
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-12 12:46:04 -0500

    completed challenge 2

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ console.log("page loaded...");
 
 function alwaysHungry(arr) {
     for(var i = 0; i < arr.length; i++)
+    console.log(arr[i]);
     if(arr[i] == "food"){
         console.log("Yummy");
     } 
@@ -15,3 +16,20 @@ alwaysHungry([3.14, "food", "pie", true, "food"]);
 // this should console log "yummy", "yummy"
 alwaysHungry([4, 1, 5, 7, 2]);
 // this should console log "I'm hungry"
+
+// 2. High Pass Filter
+// Given an array and a value cutoff, return a new array containing only the values larger than cutoff.
+
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > cutoff){
+            filteredArr.push(arr[i]);
+
+        }
+    }
+    // your code here
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // we expect back [6, 8, 10, 9]
