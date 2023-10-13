2e24854d8349b54190918e80f1dbea1f8c856f29
Rassimbm <rassimbenmhamed@outlook.com>
2023-10-12 21:48:06 -0500

    updated code

--- script.js
+++ script.js
@@ -3,33 +3,33 @@ console.log("page loaded...");
 // 1. Always Hungry
 // Write a function that is given an array and each time the value is "food" it should console log "yummy". If "food" was not present in the array console log "I'm hungry" once.
 
-// function alwaysHungry(arr) {
-//     for(var i = 0; i < arr.length; i++)
-//     console.log(arr[i]);
-//     if(arr[i] == "food"){
-//         console.log("Yummy");
-//     } 
-//     if(arr[i] !== "food") console.log("I'm hungry")
-// }
+function alwaysHungry(arr) {
+    for(var i = 0; i < arr.length; i++)
+    console.log(arr[i]);
+    if(arr[i] == "food"){
+        console.log("Yummy");
+    } 
+    if(arr[i] !== "food") console.log("I'm hungry")
+}
 
-// alwaysHungry([3.14, "food", "pie", true, "food"]);
-// alwaysHungry([4, 1, 5, 7, 2]);
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+alwaysHungry([4, 1, 5, 7, 2]);
 
 // 2. High Pass Filter
 // Given an array and a value cutoff, return a new array containing only the values larger than cutoff.
 
-// function highPass(arr, cutoff) {
-//     var filteredArr = [];
-//     for(var i = 0; i < arr.length; i++){
-//         if(arr[i] > cutoff){
-//             filteredArr.push(arr[i]);
+function highPass(arr, cutoff) {
+    var filteredArr = [];
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] > cutoff){
+            filteredArr.push(arr[i]);
 
-//         }
-//     }
-//     return filteredArr;
-// }
-// var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
-// console.log(result);
+        }
+    }
+    return filteredArr;
+}
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result);
 
 // 3. Better than average
 // Given an array of numbers return a count of how many of the numbers are larger than the average.
